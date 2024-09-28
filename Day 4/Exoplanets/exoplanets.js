6b79572bd534a5d2b163ee02981ee65e5f28cc10
Abhimanyu9539 <iabhimanyu31@gmail.com>
2024-09-28 18:39:49 +0530

    Exoplanets : finding planet names

--- Day 4/Exoplanets/exoplanets.js
+++ Day 4/Exoplanets/exoplanets.js
@@ -24,4 +24,7 @@ fs.createReadStream('kepler_data.csv')
   })
   .on('end', () => {
     console.log(`${habitablePlanets.length} habitable planets found!`);
+    console.log(habitablePlanets.map((planet) =>{
+      return planet['kepler_name'];
+    } ))
   });
