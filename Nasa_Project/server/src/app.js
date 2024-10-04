c1e61f1d00f904c381c35bdd3aa2c4e4f60e9fcf
Abhimanyu9539 <iabhimanyu31@gmail.com>
2024-10-04 22:48:04 +0530

    prod build of project

--- Nasa_Project/server/src/app.js
+++ Nasa_Project/server/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
-
+const path = require('path');
 
 const app = express();
 
@@ -10,6 +10,12 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  });
+  
 
 module.exports = app;
