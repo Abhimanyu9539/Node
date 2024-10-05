d3a56a45d632c0490505935157c16730f7b02274
Abhimanyu9539 <iabhimanyu31@gmail.com>
2024-10-05 22:50:59 +0530

    Launches API And Routes

--- Nasa_Project/server/src/app.js
+++ Nasa_Project/server/src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
+
 const path = require('path');
+const morgan  = require('morgan');
 
 const app = express();
 
@@ -9,9 +12,12 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
---
+++ Nasa_Project/server/src/models/launches.model.js
@@ -0,0 +1,17 @@
+const launch = {
+    flightNumber : 200,
+    mission : 'KEX', 
+    rocket : 'EX ES', 
+    launchDate : new Date('December 26, 2030'),
+    destination : 'kepler-442 b',
+    customer :[ 'NASA', 'ZTN'] , 
+    upcoming : true,
+    success: true
+};
+
+const launches = new Map();
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+    launches,
+};
