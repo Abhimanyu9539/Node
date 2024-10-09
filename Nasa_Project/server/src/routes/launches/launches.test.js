6395848000984db795fd1135933842cb5ab8457f
Abhimanyu9539 <iabhimanyu31@gmail.com>
2024-10-09 22:21:58 +0530

    API Test using supertest

--- Nasa_Project/server/src/routes/launches/launches.test.js
+++ Nasa_Project/server/src/routes/launches/launches.test.js
@@ -1,13 +1,32 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET /launches', () => {
-    test('It should respond with 200 success', () =>{
-        const response = 200;
-        expect(response).toBe(200);
+    test('It should respond with 200 success', async () =>{
+        const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200);
+        
     });
 });
 
 describe('Test POST /launches', () => {
-    test('It should respond with 200 success', () => {
+    test('It should respond with 201  Created',  async () => {
+        const response = await request(app)
+            .post('/launches')
+            .send({
+                mission : 'ABC D',
+                rocket : 'ABC n',
+                target : 'Alpha Centauri b', 
+                launchDate : 'May 31, 2079'
+            })
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(201);
 
+        expect(response.body).toMatchObject({
+
+        });
     });
     test('It should catch missing required properties', ()=> {});
     test('It should catch invalid dates', () => {});
